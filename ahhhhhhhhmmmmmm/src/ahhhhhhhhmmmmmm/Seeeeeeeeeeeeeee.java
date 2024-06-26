672ea9166dd65273439f322d3d6b8367d4eb135f
HP <HP@DESKTOP-I8PMKF7>
2024-06-26 10:19:58 +0530

    first commit

---
+++ ahhhhhhhhmmmmmm/src/ahhhhhhhhmmmmmm/Seeeeeeeeeeeeeee.java
@@ -0,0 +1,10 @@
+package ahhhhhhhhmmmmmm;
+
+public class Seeeeeeeeeeeeeee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hlooo");
+	}
+
+}
